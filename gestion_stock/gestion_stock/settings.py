600a643ab481fbf4354bb51004d0f173ee12ec34
Morsia Hinimdou guitdam <hinimdoumorsia@gmail.com>
2026-04-17 09:25:03 +0100

    Update settings.py

--- gestion_stock/gestion_stock/settings.py
+++ gestion_stock/gestion_stock/settings.py
@@ -94,5 +94,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hinimdoumorsia@gmail.com'  # Remplacez par votre email
-EMAIL_HOST_PASSWORD = 'gssa mvhj zswt ykuc'  # Mot de passe applicatif Gmail
+EMAIL_HOST_USER = EMAIL_HOST_USER 
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD 
